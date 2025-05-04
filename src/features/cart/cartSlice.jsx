0e3bac88debad28f4668e54fe5e3e804e57f75e6
sanish <s.karki1994@gmail.com>
2025-05-04 16:54:41 +0545

    visual design completed, Clear cart began

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -2,9 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
 
 const initialState = {
-  cartItems: cartItems,
-  amount: 10,
-  total: 0,
+  cartItems: cartItems, //individual cart items
+  amount: 10, // no of cart items
+  total: 1000, // total of all prices
   isLoading: true,
 };
 
